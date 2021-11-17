bab02e556b98a339d481b3f257a2599cd882ab5e
Huy Nguyễn <nhocksock225@gmail.com>
2021-11-17 18:24:34 +0700

    update api

--- ecommerceAPI/routes/product.js
+++ ecommerceAPI/routes/product.js
@@ -55,7 +55,7 @@ router.get("/find/:id", async (req, res) => {
 // GET ALL PRODUCTS
 router.get("/", async (req, res) => {
     const qNew = req.query.new;
-    const qCategory = req.query.category;
+    const qCategory = req.query.categories;
     try {
         let products;
         if(qNew) {
